44e5eccd4e3ac1fcd9dd26db44d7adc4fb5c32c6
Jibril Omar <jibrilosman404@gmail.com>
2024-05-24 23:06:07 -0400

    Update server.js

--- serverAPI/server.js
+++ serverAPI/server.js
@@ -19,7 +19,7 @@ const DATABASE = process.env.MONGODB_URI;
 app.use(express.json());
 app.use(cors(
     {
-        origin: 'https://hire-portal-frontend.vercel.app/',
+        origin: 'https://hire-portal-frontend.vercel.app',
         credentials: true,
     }
 ));
