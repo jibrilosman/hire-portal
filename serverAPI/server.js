0256e51d409988fb179f04dbc6cf023deed4f1a3
jibrilosman <jibrilosman404@gmail.com>
2024-05-25 01:01:25 -0400

    modified

--- serverAPI/server.js
+++ serverAPI/server.js
@@ -19,21 +19,19 @@ const DATABASE = process.env.MONGODB_URI;
 app.use(express.json());
 
 const allowedOrigins = ['http://localhost:3000', 'https://hire-portal-frontend.vercel.app'];
-const corsOptions = {
+app.use(cors({
   origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps or curl requests)
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) {
       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
     return callback(null, true);
-  },
-  methods: 'GET,HEAD,OPTIONS,POST,PUT',
-  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-};
+  }
+}));
 
-app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
