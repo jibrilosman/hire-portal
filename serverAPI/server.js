e9b1d0b352f2979d30a9056a89457bf527a57c75
Jibril Omar <jibrilosman404@gmail.com>
2024-05-26 23:28:51 -0400

    Update server.js

--- serverAPI/server.js
+++ serverAPI/server.js
@@ -25,7 +25,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 // Connect to MongoDB
 mongoose.connect(DATABASE)
