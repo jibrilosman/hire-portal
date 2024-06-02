3aa233c194a451e8297d520ebed887d174b453b5
jibrilosman <jibrilosman404@gmail.com>
2024-06-02 16:35:40 -0400

    origin cors updated

--- serverAPI/server.js
+++ serverAPI/server.js
@@ -25,15 +25,8 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  optionsSuccessStatus: 200,
-  credentials: true // if you need to handle cookies
+  origin: allowedOrigins,
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
